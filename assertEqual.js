79f9acffdb224ea6371c05681e3380b04e103908
victor poirier <poiriervictor99@gmail.com>
2020-02-03 19:28:17 +0000

    updated strings with string literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🆗 Assertion passed" + actual + " === " + expected);
+    console.log(`🆗 Assertion passed ${actual} === ${expected}`);
   } else {
-    console.log("❌ Assertion failed" + actual + " !== " + expected);
+    console.log(`❌ Assertion failed ${actual} !== ${expected}`);
   }
 };
 

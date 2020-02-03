a79a3fee331150b49d7aee1d8a8e5051ee4767db
victor poirier <poiriervictor99@gmail.com>
2020-02-03 19:12:03 +0000

    added assert equal function under assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🆗 Assertion passed" + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion failed" + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);

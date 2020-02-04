5d34d25996e44245a5b1a0616888b5e4201d6a3c
victor poirier <poiriervictor99@gmail.com>
2020-02-04 21:38:58 +0000

    added a assertArraysEquals function to print to console the result of a comparison between two arrays.

---
+++ assertArraysEquals.js
@@ -0,0 +1,17 @@
+const eqArrays = function(arr1, arr2) {
+  let longestLength = arr1.length > arr2.length ? arr1.length : arr2.length;
+  for (let i = 0; i < longestLength; i++) {
+    if (!(arr1[i] === arr2[i])) {
+      return false
+    }
+  }
+  return true;
+};
+
+const assertArraysEquals = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🆗 Assertion passed ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion failed ${actual} !== ${expected}`);
+  }
+};
